ea7bf6859872357488817bcbf434f6281a372fc3
James910066 <james.dinser@gmail.com>
2022-06-12 12:00:15 -0400

    Added Lab05 Ship Cost Calculator

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,39 @@
+public class ShipCostCalculator
+{
+    public static void main(String[] args)
+    {
+        //num itemPrice
+        //num SHIPPING_RATE = .02
+        //num FREE_SHIP_VAL = 100
+        //num totalCost
+        //output "Enter the price of the item"
+        //input itemPrice
+        //If itemPrice >= PRICE_THRESHOLD then
+        //shippingCost = itemPrice * PRICE_THRESHOLD
+        //totalCost = shippingCost + itemPrice
+        //output "The cost of shipping is “ + shippingCost + “ and the total cost is “ +  totalCost"
+        //Else
+        //output "Your shipping is free"
+        //endIF
+
+        int itemPrice = 90;
+        final double SHIPPING_RATE = .02;
+        final int FREE_SHIP_VALUE = 100;
+        double totalCost;
+        double shippingCost;
+
+        if (itemPrice >= FREE_SHIP_VALUE)
+        {
+            System.out.println("Your shipping is free");
+        }
+        else
+       {
+            shippingCost = itemPrice * SHIPPING_RATE;
+            totalCost = shippingCost + itemPrice;
+            System.out.println("The cost of shipping is " + shippingCost + " and total cost of your purchase is " + totalCost);
+       }
+
+
+    }
+
+}
